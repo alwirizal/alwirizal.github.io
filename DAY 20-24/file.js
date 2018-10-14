94fbb6cdddb6235d92801ef821339d1d421a5957
unknown <alwirizal532@gmail.com>
2018-10-15 06:51:20 +0800

    DAY 10-24

---
+++ DAY 20-24/file.js
@@ -0,0 +1,23 @@
+function myBnng(){
+    document.getElementById("myBnng1").style.color = "red"
+}
+
+function myBc() {
+    document.getElementById("myBc1").style.color = "blue"
+}
+
+function myOp() {
+    document.getElementById("myOp1").style.color = "green"
+}
+
+function myTg(){
+    document.getElementById("myTg1").style.color = "coral"
+}
+
+function myBnha() {
+    document.getElementById("myBnha1").style.color = "greenyellow"
+}
+
+function myOv() {
+    document.getElementById("myOv1").style.color = "darkturquoise"
+}
